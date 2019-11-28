2e04ed23adc4920f620dc7fc69de560ac7b189d1
MelodyLiu012 <57297187+MelodyLiu012@users.noreply.github.com>
2019-11-27 23:36:48 -0800

    -

--- One Eighth Life Crisis Simulator/One-Eighth-Life Crisis.cpp
+++ One Eighth Life Crisis Simulator/One-Eighth-Life Crisis.cpp
@@ -1,5 +1,8 @@
-// One-Eighth-Life Crisis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+Note to self:
+- Change timing for drawing text to be based on clock()
+*/
+
 
 #include "pch.h"
 #define OLC_PGE_APPLICATION
@@ -27,16 +30,23 @@ private:
 	//Game
 	bool gameRunning = false;
 	int textOpacity = 0;
-	olc::Pixel textPixel;
+	int nextOpacity = 0;
+	void drawNextIndicator();
+	void drawDialoguePage(std::string dialogueText);
+	std::string currentText = "";
+	int currentCharIndex = 0;
 	int page = 1;
 	bool dialogueStart = false;
+	
+
+	//Page 16
+	int ringFrame = 1;
 
 public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
 
-		
 		return true;
 	}
 	bool OnUserUpdate(float fElapsedTime) override
@@ -61,24 +71,35 @@ public:
 
 			//==============================================================================
 
-			/*olc::Sprite * letterA = new olc::Sprite("Letter A.png");
-			DrawSprite(20, 20, letterA, 1);*/
-
 			//Take user input
 			if (GetKey(olc::Key::X).bPressed)
 			{
-				page++;
+				if ((page == 1 && waitCounter >= 70) || (page != 1 && waitCounter >= 20)) //Prevents player from clicking too fast
+				{
+					page++;
+					waitCounter = 0;
+					nextOpacity = 0;
+					currentText = "";
+					currentCharIndex = 0;
+				}
 			}
 
-			if (page == 1) //It needs to do the "..." thing
+			if (page == 1) //First page needs special wait counter times 
 			{
-				if (waitCounter != 54)
+				if (waitCounter != 70) //Delay for "Next(X)" indicator for first page is 20 
 				{
 					waitCounter++;
 				}
-				if (waitCounter == 50)
+				if (waitCounter == 50) //Delay between title screen and dialogue start
 				{
 					dialogueStart = true;
+				}
+			}
+			else 
+			{
+				if (waitCounter != 800)
+				{
+					waitCounter++;
 				}
 			}
 
@@ -86,16 +107,107 @@ public:
 			{
 				switch (page)
 				{
-				case 1:
+				case 1: // It needs to do the "..." thing
 					if (waitCounter >= 50)
 						DrawString(40, ScreenHeight() / 2 + 50, ".", olc::BLACK, 1);
 					if (waitCounter >= 52)
-						DrawString(47, ScreenHeight() / 2 + 50, ".", olc::BLACK, 1);
-					if (waitCounter == 54)
-						DrawString(54, ScreenHeight() / 2 + 50, ".", olc::BLACK, 1);
+						DrawString(48, ScreenHeight() / 2 + 50, ".", olc::BLACK, 1);
+					if (waitCounter >= 54)
+						DrawString(56, ScreenHeight() / 2 + 50, ".", olc::BLACK, 1);
+					if (waitCounter == 70)
+						drawNextIndicator();
 					break;
 				case 2:
-					DrawString(40, ScreenHeight() / 2 + 50, "Hi.", olc::BLACK, 1);
+					drawDialoguePage("Hello.");
+					break;
+				case 3:
+					drawDialoguePage("Welcome, lost soul.");
+					break;
+				case 4:
+					drawDialoguePage("Where did you come from?");
+					break;
+				case 5:
+					drawDialoguePage("Where are you headed?");
+					break;
+				case 6:
+					drawDialoguePage("What is your future?");
+					break;
+				case 7:
+					drawDialoguePage("...");
+					break;
+				case 8:
+					drawDialoguePage("Do you have a future?");
+					break;
+				case 9:
+					drawDialoguePage("...");
+					break;
+				case 10:
+					drawDialoguePage("Well, no matter who you are...");
+					break;
+				case 11:
+					drawDialoguePage("Where you are in life right now...");
+					break;
+				case 12:
+					drawDialoguePage("If you're looking to re-experience the \n\nfeelings of your high school years...");
+					break;
+				case 13:
+					drawDialoguePage("Or to, if only for a brief moment, ease \n\nyour suffering by relating it to someone \n\nelse's...");
+					break;
+				case 14:
+					drawDialoguePage("Then you've come to the right place.");
+					break;
+				case 15:
+					drawDialoguePage("In this game, you will take the role of \n\na pitiful high school junior.");
+					break;
+				case 16:
+					// Draw alarm ring animation
+					if (waitCounter % 10 == 0) //change frame every 10 counts
+					{
+						if (ringFrame == 1)
+						{
+							ringFrame = 2;
+						}
+						else 
+						{
+							ringFrame = 1;
+						}
+					}
+					if (ringFrame == 1)
+					{
+						DrawSprite(20, 20, &olc::Sprite("AlarmRing1.jpg"), 1);
+					}
+					if (ringFrame == 2)
+					{
+						DrawSprite(20, 20, &olc::Sprite("AlarmRing2.jpg"), 1);
+					}
+
+					if (waitCounter >= 40)
+						drawDialoguePage("Oh dear, do you hear that?");
+					if (waitCounter >= 800)
+						drawDialoguePage("Oh dear, do you hear that? (The alarm \n\nwon't stop unless you move on. Don't run \n\naway from your problem and press next.)");
+					break;
+				case 17:
+					// Draw alarm ring animation
+					if (waitCounter % 10 == 0) //change frame every 10 counts
+					{
+						if (ringFrame == 1)
+						{
+							ringFrame = 2;
+						}
+						else
+						{
+							ringFrame = 1;
+						}
+					}
+					if (ringFrame == 1)
+					{
+						DrawSprite(20, 20, &olc::Sprite("AlarmRing1.jpg"), 1);
+					}
+					if (ringFrame == 2)
+					{
+						DrawSprite(20, 20, &olc::Sprite("AlarmRing2.jpg"), 1);
+					}
+					drawDialoguePage("It's the sound of your alarm.");
 					break;
 				}
 			}
@@ -148,7 +260,7 @@ void OELCSimulator::titleScreen()
 	}
 	if (waitCounter == 30)
 	{
-		DrawString(230, ScreenHeight() / 2 + 150, "Start Your Day(X)", titlePixel, 1);
+		DrawString(290, ScreenHeight() / 2 + 150, "Start(X)", titlePixel, 1);
 		if (GetKey(olc::Key::X).bPressed)
 		{
 			titleEnd = true;
@@ -166,8 +278,42 @@ void OELCSimulator::titleScreen()
 	}
 }
 
+void OELCSimulator::drawNextIndicator()
+{
+	olc::Pixel nextPixel(90, 90, 90, nextOpacity);
+	DrawString(310, 360, "Next(X)", nextPixel, 1);
+	if (nextOpacity != 255)
+	{
+		nextOpacity += 5;
+	}
+}
 
+void OELCSimulator::drawDialoguePage(std::string dialogueText)
+{
+	//Drawing dialogue
+	if (waitCounter % 2 == 0 && currentCharIndex < dialogueText.size())
+	{
+		currentText += dialogueText[currentCharIndex];
+		currentCharIndex++;
+		
+	}
+	DrawString(40, ScreenHeight() / 2 + 50, currentText, olc::BLACK, 1);
+	
 
+	//Set delay time to draw "Next(X)" indicator based on number of words
+	int numWords = 1; //initialized to 1 to account for last word before the \0
+	for (int i = 0; dialogueText[i] != '\0'; i++)
+	{
+		if (dialogueText[i] == ' ')
+		{
+			numWords++;
+		}
+	}
+	if (waitCounter >= 200 + numWords * 10) 
+	{
+		drawNextIndicator();
+	}
+}
 
 //Main Function===============================================================
 
